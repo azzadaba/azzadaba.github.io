e2975e5a128dd44d9cd513d74def06848b4fa394
Frostiae <ibraennara@gmail.com>
2024-08-01 23:25:04 -0400

    remove speech button

--- src/components/infobox.jsx
+++ src/components/infobox.jsx
@@ -16,35 +16,6 @@ export default function Infobox(props) {
         <Bigbutton key={button.text} name={button.text} link={button.link}/>
     ) : null;
 
-
-    const synth = window.speechSynthesis;
-
-    function findRoot(root) {
-        if (typeof root == "string" || root == undefined) {
-            if (root != undefined) {
-                const utter = new SpeechSynthesisUtterance(root);
-                utter.voice = synth.getVoices()[10];
-                synth.speak(utter);
-            }
-            return;
-        }
-
-        if (root.props != undefined) {
-            if (Array.isArray(root.props.children)) {
-                for (let prop of root.props.children) {
-                    findRoot(prop);
-                }
-            }
-            else {
-                findRoot(root.props.children);
-            }
-        }
-    }
-
-    function speak() {
-        findRoot(props.data.text);
-    }
-
     addEventListener("resize", () => { setScreenWidth(window.innerWidth); });
 
     return (
@@ -87,7 +58,6 @@ export default function Infobox(props) {
                 }
             </div>
             <div className="infopage">
-                <button className="btn-access" onClick={speak}> <img src="speaker.png" alt="speak" /> </button>
                 {props.data.text != undefined &&
                     props.data.text
                 }
