2f72b7f7239f05c911f0fb345054f4f252d78964
azzadaba <145408950+azzadaba@users.noreply.github.com>
2025-02-05 13:27:18 -0500

    Update etymology.jsx
    removed centuries

--- src/routes/etymology.jsx
+++ src/routes/etymology.jsx
@@ -13,7 +13,7 @@ export default function Etymology() {
           <br /><br />
           The word "etymology" itself comes from the Greek words "etymon," meaning "true sense," and "logia," meaning "study" or "science." Therefore, etymology can be understood as the science or study of the true sense or true meaning of words. It involves analyzing base elements, prefixes, suffixes, and other linguistic components that contribute to a word's meaning.
           <br /><br />
-          It helps students understand the connections between words that share a base element and sheds light on how words have been shaped by human communication over centuries.</p>
+          It helps students understand the connections between words that share a base element and sheds light on how words have been shaped by human communication over.</p>
       </div>
     )
   }
